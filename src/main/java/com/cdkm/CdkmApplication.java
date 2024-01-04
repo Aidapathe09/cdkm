9e0fd968b259e4e411c6c810eec4bd95557d9e3d
Aidapathe09 <aidapathediouf@gmail.com>
2024-01-04 22:26:53 +0000

    initialisation

---
+++ src/main/java/com/cdkm/CdkmApplication.java
@@ -0,0 +1,13 @@
+package com.cdkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdkmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdkmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdkm/CdkmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdkm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdkmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
